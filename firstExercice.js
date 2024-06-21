4d757e8a8dff7f69e43a81991446bcbd190ff68a
Yassine Mazhare <137923652+Yassine-64@users.noreply.github.com>
2024-06-21 12:04:16 +0100

    Create firstExercice.js

---
+++ firstExercice.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $("#redButton").click(function() {
+        $("#text").css("color", "red");
+    });
+
+    $("#greenButton").click(function() {
+        $("#text").css("color", "green");
+    });
+
+    $("#blueButton").click(function() {
+        $("#text").css("color", "blue");
+    });
+});
